13b478bdf846b5b0243a5d166da0c05c7e667d21
lettucegoblin <77356082+lettucegoblin@users.noreply.github.com>
2023-09-28 15:17:37 -0400

    switch to express

--- server/index.js
+++ server/index.js
@@ -1,13 +1,12 @@
-// vanilla node server
-const http = require('http');
+// express.js
+const express = require('express');
+const app = express();
 const PORT = 3000;
 
-const server = http.createServer((req, res) => {
-  res.end('Hello World');
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
-console.log("1. Trying to start server.");
-server.listen(PORT, () => {
-  console.log(`2. Server listening on port http://localhost:${PORT}`);
+app.listen(PORT, () => {
+  console.log(`server listening on port ${PORT}`);
 });
-console.log("3. end of file.");
