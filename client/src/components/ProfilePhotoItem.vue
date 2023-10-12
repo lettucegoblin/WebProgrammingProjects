d35e239ae8cf44d6ea6501b99e756c8dcb176278
lettucegoblin <77356082+lettucegoblin@users.noreply.github.com>
2023-10-12 02:09:49 -0400

    render still having issues. different pathing.

--- client/src/components/ProfilePhotoItem.vue
+++ client/src/components/ProfilePhotoItem.vue
@@ -1,18 +1,17 @@
 <template>
-  <img :src="profilePhoto" onerror="this.onerror=null; this.src='/src/assets/profile_photos/p_Default.webp'">
+  <img :src="profilePhoto" :onerror="onError">
 </template>
 
 <script setup lang="ts">
 import { computed } from 'vue';
-const defaultPath = '/src/assets/profile_photos/'
 
 const onError = (event: Event) => {
   const target = event.target as HTMLImageElement;
-  target.src = 'p_Default.webp';
+  target.src = '/src/assets/profile_photos/p_Default.webp';
 }
 
 const profilePhoto = computed<string>(() => {
-  return new URL(`${defaultPath}p_${props.userId+1}.webp`, import.meta.url).href;
+  return new URL(`/src/assets/profile_photos/p_${props.userId+1}.webp`, import.meta.url).href;
 })
 const props = defineProps({
   userId: {
